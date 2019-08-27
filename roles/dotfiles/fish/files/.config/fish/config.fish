71d02c3e5c3ec49473c7431003da36b980103a0d
emag <lanabe.lanabe@gmail.com>
2019-08-27 21:22:49 +0900

    Add nodenv for fedora

--- roles/dotfiles/fish/files/.config/fish/config.fish
+++ roles/dotfiles/fish/files/.config/fish/config.fish
@@ -4,3 +4,6 @@ direnv hook fish | source
 # jenv
 set PATH $HOME/.jenv/bin $PATH
 status --is-interactive; and source (jenv init -|psub)
+
+# nodenv
+status --is-interactive; and source (nodenv init -|psub)
